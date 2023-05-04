b95dd4f53f4dde3c94b160443386618199b88cdb
Binod Bhandari <binodcoder@gmail.com>
2023-05-04 14:41:02 +0545

    my custom dropdown with scroll

---
+++ lib/custom_dropdown.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class CustomDropDownWidget extends StatefulWidget {
+  const CustomDropDownWidget({super.key});
+
+  @override
+  State<CustomDropDownWidget> createState() => _CustomDropDownWidgetState();
+}
+
+class _CustomDropDownWidgetState extends State<CustomDropDownWidget> {
+  bool isOpen = false;
+  String selectOption = 'Select Option';
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drop Down'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 8,
+            ),
+            InkWell(
+              onTap: () {
+                isOpen = !isOpen;
+                setState(() {});
+              },
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(color: Colors.grey.shade300),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text(selectOption), Icon(isOpen ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down)],
+                  ),
+                ),
+              ),
+            ),
+            const Divider(
+              height: 2,
+            ),
+            if (isOpen)
+              Expanded(
+                child: ListView(
+                  primary: true,
+                  shrinkWrap: true,
+                  children: numbers
+                      .map(
+                        (e) => Container(
+                          decoration: BoxDecoration(color: selectOption == e.toString() ? Colors.pink : Colors.grey.shade300),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: InkWell(
+                              onTap: () {
+                                selectOption = e.toString();
+                                isOpen = false;
+                                setState(() {});
+                              },
+                              child: Text(
+                                e.toString(),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:custom_dropdown_demo_flutter/custom_dropdown.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: CustomDropDownWidget()));
+}
